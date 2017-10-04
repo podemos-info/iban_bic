f3767b56b7ae02779d62b5f2bdc94734a9eefd1f
Leonardo Diez <leiodd@gmail.com>
2017-10-04 13:34:54 +0200

    Loaded IBAN validator

---
+++ lib/active_model/validations/iban_validator.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "active_model/validations"
+require "iban_bic"
+
+# ActiveModel Rails module.
+module ActiveModel
+  # ActiveModel::Validations Rails module. Contains all the default validators.
+  module Validations
+    class IbanValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        if !IbanBic.parse(value)
+          record.errors.add(attribute, :invalid_format)
+        elsif !IbanBic.valid_check?(value)
+          record.errors.add(attribute, :invalid_check)
+        elsif !IbanBic.valid_country_check?(value)
+          record.errors.add(attribute, :invalid_country_check)
+        end
+      end
+    end
+  end
+end
--- lib/iban_bic.rb
+++ lib/iban_bic.rb
@@ -4,5 +4,6 @@ require "yaml"
 require "iban_bic/version"
 require "iban_bic/configuration"
 require "iban_bic/core"
+require "active_model/validations/iban_validator"
 require "iban_bic/engine" if defined?(Rails::Engine)
 require "iban_bic/defaults"
