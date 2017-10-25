e2dacaf318d56550b493bf7c15c9138f522e209e
Leonardo Diez <leiodd@gmail.com>
2017-10-25 16:05:28 +0200

    Use SimpleCov formatter only for CI tests runs.

--- spec/tests_helper.rb
+++ spec/tests_helper.rb
@@ -3,7 +3,9 @@
 require "simplecov"
 SimpleCov.start
 
-require "codecov"
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+if ENV["CI"]
+  require "codecov"
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 require "support/tasks"
