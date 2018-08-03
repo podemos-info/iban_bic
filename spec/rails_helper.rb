f3a48cc16f9ad06bb20b824d2d831c170e5df2e6
Leonardo Diez <leiodd@gmail.com>
2018-08-03 18:20:40 +0200

    Bump dependencies

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,4 +36,4 @@ RSpec.configure do |config|
 end
 
 # Run any available migration
-ActiveRecord::Migrator.migrate File.expand_path("test_app/db/migrate/", __dir__)
+ActiveRecord::MigrationContext.new("test_app/db/migrate/").migrate
